88c279e6f4f2a540e9c75d7b0cc9b2df598e4868
Mohammad Shahhaidari <99161833+MSh-786@users.noreply.github.com>
2026-03-18 18:59:48 -0400

    remove unwanted function declarations

--- helper/vga.h
+++ helper/vga.h
@@ -14,7 +14,7 @@ extern volatile int * character_ctrl_ptr;
 extern volatile int pixel_buffer_start;
 extern volatile int* pixel_ctrl_ptr;
 
-void vga_text(int x, int y, char * text_ptr) 
+void vga_text(int x, int y, char * text_ptr);
 void plotPixel(point p, short int line_color);
 void clearScreen(void);
 void waitForVsync(void);
@@ -22,7 +22,5 @@ void swapXY(point* p);
 void swap2Points(point* p0, point* p1);
 void drawLine(point p0, point p1, short int color);
 void fillBox(point p, int size, short int color);
-void drawChar(point top_left, char c, short int color, int scale);
-void drawText(point top_left, const char *text, short int color, int scale);
 
 #endif

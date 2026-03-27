a5fef98dc55fd234ae0dc3cb12486e067efe9e14
Niz Ansari <nizansari@Yousufs-MacBook-Air.local>
2026-03-26 20:26:13 -0400

    Tinkering with the button alignments and stuff

--- main.c
+++ main.c
@@ -124,9 +124,9 @@ static void drawFullFrame(
 ){
     clearRegion((point){0, 80}, 320, 160);   // wipe buttons + graph region
     vga_text(170 / 4, 86 / 4, instructions);
-    createGraphButton(button1, (point){25, 78},  time_fill,        GRAPH_COLOR);
-    createGraphButton(button2, (point){56, 78},  spectrum_fill,    GRAPH_COLOR);
-    createGraphButton(button3, (point){102, 78}, spectrogram_fill, GRAPH_COLOR);
+    createGraphButton(button1, (point){25, 76},  time_fill,        GRAPH_COLOR);
+    createGraphButton(button2, (point){56, 76},  spectrum_fill,    GRAPH_COLOR);
+    createGraphButton(button3, (point){103, 76}, spectrogram_fill, GRAPH_COLOR);
     displayCorrectGraph();
 }
 
@@ -141,9 +141,9 @@ int main(void){
     clearRegion((point){0, 0}, 320, 240);
 
     vga_text(170 / 4, 86 / 4, instructions);
-    createGraphButton(button1, (point){25, 78}, time_fill, GRAPH_COLOR);
-    createGraphButton(button2, (point){56, 78}, spectrum_fill, GRAPH_COLOR);
-    createGraphButton(button3, (point){102, 78}, spectrogram_fill, GRAPH_COLOR);
+    createGraphButton(button1, (point){25, 76}, time_fill, GRAPH_COLOR);
+    createGraphButton(button2, (point){56, 76}, spectrum_fill, GRAPH_COLOR);
+    createGraphButton(button3, (point){103, 76}, spectrogram_fill, GRAPH_COLOR);
 
     displayCorrectGraph();
 

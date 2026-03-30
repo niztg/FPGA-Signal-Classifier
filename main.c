afed4394bc04f9d1d3e175dd075102cbae5a0d38
Niz Ansari <nizansari@Yousufs-MacBook-Air.local>
2026-03-30 14:35:09 -0400

    Trying to get the vga text on screeen

--- main.c
+++ main.c
@@ -234,13 +234,13 @@ static void drawFullFrame(
         } else {
             sprintf(prediction_text, "Prediction: NOT AUTHORIZED. CONFIDENCE: %.2f%%    ", (1 - percent) * 100);
         }
-        vga_text(6, 12, prediction_text);
+        vga_text(12, 12, prediction_text);
     }
 
     if (is_channel_1){
-        vga_text(6, 19, "CH. 1");
+        vga_text(12, 76, "CH. 1");
     } else{
-        vga_text(6, 19, "CH. 2");
+        vga_text(12, 76, "CH. 2");
     }
 }
 
@@ -263,7 +263,7 @@ int main(void){
 
     displayCorrectGraph();
 
-    vga_text(6, 19, "CH. 1");
+    vga_text(6, 76, "CH. 1");
 
     static bool ps2_break_pending = false;
     static bool ps2_extend_pending = false;
